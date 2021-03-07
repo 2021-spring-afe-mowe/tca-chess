dfa62488a90082b44cd79b8325b0a21e45fc95d4
emnekola <enekola@madisoncollege.edu>
2021-03-06 18:37:02 -0600

    Start other views

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,23 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./home/home.module')
+      .then( m => m.HomePageModule)
   },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },
+  {
+    path: 'current-game',
+    loadChildren: () => import('./current-game/current-game.module')
+      .then( m => m.CurrentGamePageModule)
+  },
+  {
+    path: 'my-games',
+    loadChildren: () => import('./my-games/my-games.module')
+      .then( m => m.MyGamesPageModule)
   },
 ];
 
---
+++ src/app/current-game/current-game.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-current-game',
+  templateUrl: './current-game.page.html',
+  styleUrls: ['./current-game.page.scss'],
+})
+export class CurrentGamePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    console.log("current game here")
+  }
+
+}
