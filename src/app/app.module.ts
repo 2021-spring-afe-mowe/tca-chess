d57695dc8486a889e401833b61c9af98e3209a84
emnekola <58869230+emnekola@users.noreply.github.com>
2021-04-12 15:17:29 -0500

    Added angular PWA
    Messed up icons, need to do this first next time.
    
    ng add @angular/pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,8 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
@@ -20,7 +22,12 @@ import { AppRoutingModule } from './app-routing.module';
     BrowserModule, IonicModule.forRoot(), AppRoutingModule, IonicStorageModule.forRoot({
      name: '__mydb',
      driverOrder: [Drivers.LocalStorage]
-   })],
+   }), ServiceWorkerModule.register('ngsw-worker.js', {
+  enabled: environment.production,
+  // Register the ServiceWorker as soon as the app is stable
+  // or after 30 seconds (whichever comes first).
+  registrationStrategy: 'registerWhenStable:30000'
+})],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
